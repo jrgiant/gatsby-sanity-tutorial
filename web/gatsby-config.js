b8dd5c48f8086853709ec0e8cd704f167d0f4dfc
JrGiant <jrgiant_git@outlook.com>
2019-12-28 16:58:55 -0600

    First working version

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -5,6 +5,16 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '4bc5zk6l',
+        dataset: 'production',
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.MY_SANITY_TOKEN,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
